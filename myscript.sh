7ed5a2a436022c6450ac658ac3a1fb309f93f504
Young Kim <young-k@protonmail.com>
2016-05-19 12:22:41 -0400

    i tried; added jquery cdn

--- myscript.sh
+++ myscript.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/expect -f
-STR="149.89.150."
-for i in {1..6}
-  spawn ssh hanzhangfrankl.wang@$STR$i
+set str "149.89.150."
+for {set i 0} {$i<6} {incr i} {
+  spawn ssh hanzhangfrankl.wang@$str$i
   expect "assword:"
   sleep 1
   send "dragons123\r"
-  command1
-  command2
-  commandN
+  expect "$ "
+  send "cd ~/Desktop\r"
+  expect "$ "
+  send "echo 'CPU Name:' >> output.txt\r"
+  expect "$ "
+  send "hostname >> output.txt\r"
+  expect "$ "
+  send "echo 'CPU Info: ' >> output.txt\r"
+  expect "$ "
+  send "lscpu >> output.txt\r"
+  expect "$ "
+  send "echo 'Version: ' >> output.txt\r"
+  expect "$ "
+  send "cat /proc/version >> output.txt\r"
+  expect "$ "
+  send "echo '=====' >> output.txt\r"
+  expect "$ "
+  send "echo '' >> output.txt\r"
+}
