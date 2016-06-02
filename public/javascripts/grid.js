60fabe92c75f19c750090d140d47c4a24a2c0197
Young Kim <young-k@protonmail.com>
2016-06-02 09:57:51 -0400

    what

--- public/javascripts/grid.js
+++ public/javascripts/grid.js
@@ -2,12 +2,12 @@ var svg = document.getElementById("mysvg");
 
 //how to read from database
 var computers = [];
-var databaseComp = [{"name":"cs101","os":"windows","date":"12-9-41","xcor":0,"ycor":0, "issue":2},{"name":"cs102","os":"windows","date":"12-9-41","xcor":0,"ycor":1, "issue":1},{"name":"cs111","os":"windows","date":"12-9-41","xcor":1,"ycor":1, "issue":0},{"name":"cs101","os":"windows","date":"12-9-41","xcor":2,"ycor":0, "issue":0},{"name":"cs102","os":"windows","date":"12-9-41","xcor":3,"ycor":0, "issue":1},{"name":"cs111","os":"windows","date":"12-9-41","xcor":1,"ycor":0, "issue":0},{"name":"cs102","os":"windows","date":"12-9-41","xcor":3,"ycor":1, "issue":0},{"name":"cs111","os":"windows","date":"12-9-41","xcor":2,"ycor":1, "issue":0}];       
+var databaseComp = [{"name":"cs101","os":"windows","date":"12-9-41","xcor":0,"ycor":0, "issue":2},{"name":"cs102","os":"windows","date":"12-9-41","xcor":0,"ycor":1, "issue":1},{"name":"cs111","os":"windows","date":"12-9-41","xcor":1,"ycor":1, "issue":0},{"name":"cs101","os":"windows","date":"12-9-41","xcor":2,"ycor":0, "issue":0},{"name":"cs102","os":"windows","date":"12-9-41","xcor":3,"ycor":0, "issue":1},{"name":"cs111","os":"windows","date":"12-9-41","xcor":1,"ycor":0, "issue":0},{"name":"cs102","os":"windows","date":"12-9-41","xcor":3,"ycor":1, "issue":0},{"name":"cs111","os":"windows","date":"12-9-41","xcor":2,"ycor":1, "issue":0}];
 
 var Computer = function(CompDictionary) {
-    
+
     var issueColors = ["green","yellow","red"];//list of possible colors
-    
+
     var width = $(window).width()/15;//might depend on screen height
     var height = $(window).width()/15;//might depend on screen height
     var xcor = CompDictionary['xcor']*($(window).width()/15 + 10);//depends on index in the grid
@@ -28,7 +28,7 @@ var Computer = function(CompDictionary) {
     comp.setAttribute("stroke", "black");
     //comp.setAttribute("style", "fill:" + color + ";stroke:black;stroke-width:5;opacity:0.5");
     comp.setAttribute("class", "tooltip");
-    
+
     var compInfo = document.createElementNS("http://www.w3.org/2000/svg", "text");
     compInfo.setAttribute("class", "tooltiptext");
     compInfo.setAttribute("x",xcor + 15);
@@ -36,7 +36,7 @@ var Computer = function(CompDictionary) {
     compInfo.setAttribute("fill","black");
     compInfo.setAttribute("pointer-events","none");
     compInfo.innerHTML = infoString;
-    
+
     compLink.appendChild(comp);
     compLink.appendChild(compInfo);
     svg.appendChild(compLink);
@@ -51,14 +51,14 @@ var Computer = function(CompDictionary) {
 };
 
 var populateRoom = function(){
-  //goes through database and create a room object with computers  
+  //goes through database and create a room object with computers
   var newComp;
   //populate from the database
   for (var i = 0; i < databaseComp.length; i++) {
       newComp = Computer(databaseComp[i]);
       computers.push(newComp);
   }
-  
+
 };
 
 populateRoom();
--- ssh package/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-hostname >> output.txt
-hostname -I >> output.txt
-cat /sys/devices/virtual/dmi/id/sys_vendor >> output.txt
-
-{ cat /proc/version; echo "!!"; } | tr "\n" " " >> output.txt
-echo '==' >> output.txt
