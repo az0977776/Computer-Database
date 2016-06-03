a8a9f037705c8e877bbe84e41e57f888ded9e67e
Young Kim <young-k@protonmail.com>
2016-06-02 21:02:34 -0400

    finished add issue page

--- routes/edit.js
+++ routes/edit.js
@@ -9,11 +9,26 @@ router.get('/', function(req, res, next) {
 /* POST signup page. */
 router.post('/', function(req, res, next) {
   console.log(req.body.id);
+  console.log(req.body.level);
   console.log(req.body.fill);
 
-  var database = require('../databaseModule.js');
-  database.init();
-  res.render('edit');
+  var check = /cslab[\d]+-[\d]+/;
+
+  if(!check.test(req.body.id)){
+    console.log('here');
+    res.render('edit', {error: "Error: ID not in correct format! e.g. 'cslab1-5'"})
+  }
+  else if(req.body.fill == "") {
+    res.render('edit', {error: "Error: No Description Entered For Issues!"});
+  }
+  else if(req.body.level == undefined){
+    res.render('edit', {error: "Error: No Level Chosen!"});
+  }
+  else {
+    var database = require('../databaseModule.js');
+    database.init();
+    res.render('edit');
+  }
 });
 
 module.exports = router;
